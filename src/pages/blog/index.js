1d6b54afd2b8b8258387c1ac37d3af03249caa74
Santi <santipalenque@gmail.com>
2020-06-10 21:15:39 -0300

    add schedule lite

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,3 +1,5 @@
+import 'schedule-lite/dist/index.css';
+
 import React from 'react'
 import Loadable from "@loadable/component"
 
@@ -5,18 +7,34 @@ import Layout from '../../components/Layout'
 
 import YoutubeVideoComponent from '../../components/YoutubeVideoComponent'
 import DisqusComponent from '../../components/DisqusComponent'
-const ScheduleClientSide = Loadable(() => import('../../components/ScheduleComponent'))
+import ScheduleLite from 'schedule-lite/dist';
+const ScheduleClientSide = Loadable(() => import('../../components/ScheduleComponent'));
+
 
 export default class BlogIndexPage extends React.Component {
-  render() {
-    return (
-      <Layout>
-        Testing place
-        
-        <YoutubeVideoComponent videoSrcURL="https://www.youtube.com/embed/P7d1H83IcjE" />
-        <DisqusComponent />
-        <ScheduleClientSide base='blog'/>
-      </Layout>
-    )
-  }
+
+    render() {
+        const scheduleProps = {
+            apiBaseUrl: "https://api.dev.fnopen.com",
+            eventBaseUrl: "/events",
+            trackBaseUrl: "/tracks",
+            speakerBaseUrl: "/speakers",
+            roomBaseUrl: "/rooms",
+            summitId: 16,
+            landscape: true,
+            updateCallback: ev => console.log('event updated', ev),
+        };
+
+        return (
+          <Layout>
+            Testing place
+
+            {/*<YoutubeVideoComponent videoSrcURL="https://www.youtube.com/embed/P7d1H83IcjE" />
+            <DisqusComponent />
+            <ScheduleClientSide base='blog'/>*/}
+
+            <ScheduleLite {...scheduleProps} />
+          </Layout>
+        )
+    }
 }
