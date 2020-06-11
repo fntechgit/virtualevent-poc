5856e0f4010ab5965b2253a87976dcc0204a57d2
Santi <santipalenque@gmail.com>
2020-06-10 21:35:49 -0300

    use loadable

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,41 +1,36 @@
-import 'schedule-lite/dist/index.css';
+import "schedule-lite/dist/index.css";
 
-import React from 'react'
-import Loadable from "@loadable/component"
+import React from "react";
+import Loadable from "@loadable/component";
 
-import Layout from '../../components/Layout'
+import Layout from "../../components/Layout";
 
-import YoutubeVideoComponent from '../../components/YoutubeVideoComponent'
-import DisqusComponent from '../../components/DisqusComponent'
-import ScheduleLite from 'schedule-lite/dist';
-const ScheduleClientSide = Loadable(() => import('../../components/ScheduleComponent'));
-
+import YoutubeVideoComponent from "../../components/YoutubeVideoComponent";
+import DisqusComponent from "../../components/DisqusComponent";
+const ScheduleLite = Loadable(() => import("schedule-lite/dist"));
+const ScheduleClientSide = Loadable(() => import("../../components/ScheduleComponent"));
 
 export default class BlogIndexPage extends React.Component {
+  render() {
+    const scheduleProps = {
+      apiBaseUrl: "https://api.dev.fnopen.com",
+      eventBaseUrl: "/events",
+      trackBaseUrl: "/tracks",
+      speakerBaseUrl: "/speakers",
+      roomBaseUrl: "/rooms",
+      summitId: 16,
+      landscape: true,
+      updateCallback: (ev) => console.log("event updated", ev),
+    };
 
-    render() {
-        const scheduleProps = {
-            apiBaseUrl: "https://api.dev.fnopen.com",
-            eventBaseUrl: "/events",
-            trackBaseUrl: "/tracks",
-            speakerBaseUrl: "/speakers",
-            roomBaseUrl: "/rooms",
-            summitId: 16,
-            landscape: true,
-            updateCallback: ev => console.log('event updated', ev),
-        };
-        
-
-        return (
-          <Layout>
-            Testing place
-
-            {/*<YoutubeVideoComponent videoSrcURL="https://www.youtube.com/embed/P7d1H83IcjE" />
+    return (
+      <Layout>
+        Testing place
+        {/*<YoutubeVideoComponent videoSrcURL="https://www.youtube.com/embed/P7d1H83IcjE" />
             <DisqusComponent />
             <ScheduleClientSide base='blog'/>*/}
-
-            <ScheduleLite {...scheduleProps} />
-          </Layout>
-        )
-    }
+        <ScheduleLite {...scheduleProps} />
+      </Layout>
+    );
+  }
 }
