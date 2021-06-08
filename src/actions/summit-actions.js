c162c33a879265e75cdef64379d94f92f784e2f2
Román Gutierrez <roman.gutierrez@gmail.com>
2021-06-08 20:29:27 -0300

    A2A widget improvements

--- src/actions/summit-actions.js
+++ src/actions/summit-actions.js
@@ -1,24 +1,18 @@
-import {
-  getRequest,
-  createAction,
-  stopLoading,
-  startLoading,
-} from 'openstack-uicore-foundation/lib/methods';
+import { getRequest, createAction, stopLoading, startLoading } from "openstack-uicore-foundation/lib/methods";
 
-import SummitObject from '../content/summit.json';
+import SummitObject from "../content/summit.json";
 
-import { customErrorHandler } from '../utils/customErrorHandler';
+import { customErrorHandler } from "../utils/customErrorHandler";
 
-export const GET_SUMMIT_DATA = 'GET_SUMMIT_DATA';
-export const SUMMIT_PHASE_AFTER = 'SUMMIT_PHASE_AFTER'
-export const SUMMIT_PHASE_DURING = 'SUMMIT_PHASE_DURING'
-export const SUMMIT_PHASE_BEFORE = 'SUMMIT_PHASE_BEFORE'
-export const UPDATE_CLOCK = 'UPDATE_CLOCK';
-export const GET_TIME_NOW = 'GET_TIME_NOW';
-export const TIME_NOW = 'TIME_NOW';
+export const GET_SUMMIT_DATA = "GET_SUMMIT_DATA";
+export const SUMMIT_PHASE_AFTER = "SUMMIT_PHASE_AFTER";
+export const SUMMIT_PHASE_DURING = "SUMMIT_PHASE_DURING";
+export const SUMMIT_PHASE_BEFORE = "SUMMIT_PHASE_BEFORE";
+export const UPDATE_CLOCK = "UPDATE_CLOCK";
+export const GET_TIME_NOW = "GET_TIME_NOW";
+export const TIME_NOW = "TIME_NOW";
 
 export const getSummitData = () => (dispatch) => {
-
   dispatch(startLoading());
 
   return getRequest(
@@ -26,10 +20,12 @@ export const getSummitData = () => (dispatch) => {
     createAction(GET_SUMMIT_DATA),
     `${window.SUMMIT_API_BASE_URL}/api/public/v1/summits/${window.SUMMIT_ID}?expand=event_types%2C+tracks%2C+track_groups%2C+presentation_levels%2C+locations.rooms%2C+locations.floors`,
     customErrorHandler
-  )({})(dispatch).then(() => {
-    dispatch(stopLoading());
-  }).catch(e => {
-    dispatch(stopLoading());
-    return (e);
-  });
-}
+  )({})(dispatch)
+    .then(() => {
+      dispatch(stopLoading());
+    })
+    .catch((e) => {
+      dispatch(stopLoading());
+      return e;
+    });
+};
