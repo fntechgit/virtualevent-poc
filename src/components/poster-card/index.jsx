7d9863a5ff3a0198a6153c682b9e7eb853a55ee2
gcutrini <gabriel.cutrini@gmail.com>
2022-02-04 18:02:44 -0300

    Update src/components/poster-card/index.jsx
    Co-authored-by: Santi <santipalenque@gmail.com>

--- src/components/poster-card/index.jsx
+++ src/components/poster-card/index.jsx
@@ -8,7 +8,7 @@ import styles from './index.module.scss';
 
 const PosterCard = ({ poster, showDetail, canVote, toggleVote }) => {
   const [hover, setHover] = useState(false);
-  if (poster == null) return null;
+  if (!poster) return null;
   const {title, order, track, imageURL, isVoted} = poster;
   const handleClick = ev => {
       ev.preventDefault();
