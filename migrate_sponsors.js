d887d21b1929b199eba0d9ff3124e208f986bc5a
smarcet <smarcet@gmail.com>
2022-04-22 17:22:35 -0300

    Updated content
    Signed-off-by: smarcet <smarcet@gmail.com>

---
+++ migrate_sponsors.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const fs = require('fs');
+
+const file = 'src/content/sponsors.json';
+let rawdata = fs.readFileSync(file);
+let sponsors = JSON.parse(rawdata);
+//console.log(sponsors);
+
+sponsors.tierSponsors.forEach(e => {
+    e.sponsors.forEach( s => {
+        const fields = [
+            'headerImageMobile',
+            'advertiseImage',
+            'logo',
+            'headerImage',
+            'sideImage'
+        ];
+        fields.forEach(f => {
+            if(s.hasOwnProperty(f) && s[`${f}`] != ''){
+                s[`${f}`] = {file : s[`${f}`] , alt:'' };
+            }
+        })
+        console.log(s)
+    })
+})
+
+let data = JSON.stringify(sponsors, null, 2);
+
+fs.writeFile(file, data, (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+});
