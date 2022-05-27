fc79d0bba8f8ef0ce14710f9b81fe1b4f4238a34
Jared Hill <jaredbhill@gmail.com>
2022-05-27 12:00:18 -0500

    feat(my-orders-tickets): setup and scaffolding

---
+++ src/templates/my-tickets-page.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+import Layout from "../components/Layout";
+import { MyOrdersTicketsComponent } from '../components/MyOrdersTicketsComponent';
+
+const MyTicketsPage = ({ location, user, isLoggedUser }) => (
+    <Layout location={location}>
+        <div className="container">
+            <MyOrdersTicketsComponent />
+        </div>
+    </Layout>
+);
+
+const mapStateToProps = ({ userState }) => ({
+    user: userState,
+});
+
+export default connect(mapStateToProps, {})(MyTicketsPage);
